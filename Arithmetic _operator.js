40624d15f407e8e54bf9e8ce2852d65e56e180c7
DEEPANJALI <89567807+Deepanjali2915@users.noreply.github.com>
2022-11-10 16:26:48 +0530

    Add files via upload

---
+++ Arithmetic _operator.js
@@ -0,0 +1,36 @@
+// var a="123"
+// var b=123
+// console.log(a+b)
+
+
+
+// var a=123
+// var b=123
+// console.log(a+b)
+
+
+// var a="asd"
+// var b="45678"
+// console.log(a+b)
+
+
+
+// var a="asdf"
+// var b=123
+// console.log(a-b)
+
+
+// var a="erty"
+// var b=1567
+// console.log(a*b)
+
+// var a="qw"
+// var b=3
+// console.log(typeof(a**b))//number whyy?????
+// console.log(a**b)
+
+// var a="qwe"
+// var b="qwef"
+// console.log(typeof(a**b))
+// console.log(a**b)
+
